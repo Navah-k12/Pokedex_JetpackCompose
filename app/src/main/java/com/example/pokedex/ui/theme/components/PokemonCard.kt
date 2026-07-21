6ea4f12b51ac47f512303cb81cbd8486d20d926e
Navah-k12 <navahca21@gmail.com>
2026-07-21 02:09:29 -0500

    Modificamos el color de card y le colocamos elevation para que tome efecto de flotar cada card

--- app/src/main/java/com/example/pokedex/ui/theme/components/PokemonCard.kt
+++ app/src/main/java/com/example/pokedex/ui/theme/components/PokemonCard.kt
@@ -10,10 +10,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,7 +40,9 @@ fun PokemonCard(pokemon : Pokemon, onClick: ()-> Unit){
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable{onClick()}
+            .clickable{onClick()},
+        colors = CardDefaults.cardColors(Color.White),
+        elevation = CardDefaults.cardElevation(defaultElevation = 20.dp)
     ) {
         Column(
             modifier = Modifier
